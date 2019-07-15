8539dbf179dc8e0138ef0abb18b29b6af6578692
upstatedesignco <52842468+upstatedesignco@users.noreply.github.com>
2019-07-15 11:47:21 -0400

    Update index.php

--- index.php
+++ index.php
@@ -9,33 +9,7 @@
   
 </head>
 <body>
-  <div class="hero">
-    <img class="logo" src="https://i.imgur.com/SN2JZ64.png">
-    <h1 class="head">Web, branding & design for your business<h1>
-      
-      <button id="getstarted" onclick="" type="button">Get Started</button>
-      <p>
-      <input onclick="smoothScroll(document.getElementById('brand'))" id="learnmore" type=image src="https://i.imgur.com/n2yjZ60.png">
-             </p>
-<form action="form.php" method="post">
-<p>
-      <input class="fields" type="text" placeholder="Name*" name="uname" required>
-      </p>
-      <input class="fields" type="text" placeholder="Email*" name="psw" required>
-<p>
-      <button class="info" type="submit">Request Info</button>
-      </p>
-</form>
-  </div>
-  
-      
-           <div id="myModal" class="modal">
-        <!-- Modal content -->
-  <div class="modal-content">
-    <span class="close">&times;</span>
-    <p class="conthead">Contact Us</p>
-    
-    <div class="container">
+
       <p>
         <form action="form.php" method="post">
       <input class="fields" value="<?php echo $name;?>" type="text" placeholder="Name*" name="name" required>
@@ -44,14 +18,6 @@
 <p>
       <button class="info" name="submit" type="submit">Request Info</button>
   </form>
-    </div>
-  </div>
-             </div>
-      
-      
-  <div id="brand">
-    
-  </div>
-  
+ 
 </body>
 </html>
